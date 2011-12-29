ab38f4b889477d3c6b9360527957902988a5918f
Eiffel Q <eiffelqiu@gmail.com>
2011-12-29 16:48:12 +0800

    fix readme

--- test/ren_ren_test.rb
+++ test/ren_ren_test.rb
@@ -1,15 +1,20 @@
 require 'teststrap'
-require 'json'
 
 context "ren_ren" do
   setup do
-    RenRen::Base.new(RenRen::Config.access_token).api()
+    RenRen::Base.new(RenRen::Config.access_token)
   end
 
   context "user info" do
-    asserts("user info is not null") { !topic.empty? }
-    asserts("user's sex") { topic[0]['sex'] == 1 }
-    asserts("user's uid") { topic[0]['uid'] == 433516336 }
-    asserts("user's name") { topic[0]['name'] == "锐思互动" }
+    asserts("user info is not null") { !topic.api().empty? }
+    asserts("user's sex") { topic.api()[0]['sex'] == 1 }
+    asserts("user's uid") { topic.api()[0]['uid'] == 433516336 }
+    asserts("user's name") { topic.api()[0]['name'] == "锐思互动" }
   end
+  
+  context "feed info" do
+    asserts("user info is not null") { !topic.api().empty? }
+    asserts("feed info is not null") { !topic.api({:method => "feed.get", :uid => topic.api()[0]['uid']}).empty? }
+    asserts("user's name")
+  end  
 end
